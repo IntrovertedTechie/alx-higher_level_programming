ec3beef242c25e68efd5e234dfed1846950691a2
IntrovertedTechie <zillionaire244@gmail.com>
2022-12-04 12:01:59 +0100

    list-data

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for line in matrix:
-        for col in range(len(line)):
-            print(
-                "{:d}".format(line[col]),
-                end="" if col == len(line) - 1 else " ")
-        print("")
-
+    for rows in matrix:
+        print(" ".join("{:d}".format(cols) for cols in rows))
