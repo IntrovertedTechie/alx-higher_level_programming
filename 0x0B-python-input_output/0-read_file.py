3dd07cdc3ed0d714595a99116e4a40f42fee3564
introvertedtechie <Zillionaire234@gmail.com>
2023-01-10 13:26:13 +0100

    input output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains information about reading
+a UTF-8 encoded text file
+"""
+
+
+def read_file(filename=""):
+    """
+    Prints a UTF-8 file line by line
+    Args:
+        filename (str): The name of the file
+    """
+    if type(filename) is not str:
+        raise TypeError("filename must be a string")
+    with open(filename, encoding="utf-8") as text_file:
+        for line in text_file:
+            print(line, end="")
