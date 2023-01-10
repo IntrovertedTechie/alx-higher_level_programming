a18bac48c114ba3007082c775a887ce83460d14b
introvertedtechie <Zillionaire234@gmail.com>
2023-01-10 15:07:38 +0100

    I/O

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,6 +4,7 @@ This module returns the serialized __dict__
 of an object
 """
 
-def class_to_json(obj):
-    return obj.__dict__
+    
+    def class_to_json(obj):
+           return obj.__dict__
 
