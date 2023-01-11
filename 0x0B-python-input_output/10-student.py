89de49cd710bbbd49988eef388e22f2cda4e0f88
introvertedtechie <Zillionaire234@gmail.com>
2023-01-11 02:25:16 +0100

    O/1

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,43 +1,28 @@
 #!/usr/bin/python3
 """
-This module describes the perfect Student
+This is the "Student"  class module
 """
 
 
 class Student:
-    """
-    Here's the layout of a Student
-    """
+    """A class that defines a Student"""
     def __init__(self, first_name, last_name, age):
-        """
-        Initialization of a Student
-        Args:
-            first_name (str): The Student's first name
-            last_name (str): The Student's last name
-            age (int): The Student's age
-        """
+        """Student object instance constructor"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """
-        The dictionary representation of a Student
-        Args:
-            attrs (list): List of attributes to retrieve
+           If attrs is a list of strings, only attribute names
+           contained in this list must be retrieved. Otherwise,
+           all attributes must be retrieved
         """
-        if type(attrs) is not list:
-            return self.__dict__
-            
-        for attr in attrs:
-            if type(attr) is not str:
-                return self.__dict__
-            
-
-        attr_dict = {}
-        for attr in attrs:
-            try:
-                attr_dict[attr] = self.__dict__[attr]
-            except:
-                continue
-        return attr_dict
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            new = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    new[key] = value
+            return (new)
