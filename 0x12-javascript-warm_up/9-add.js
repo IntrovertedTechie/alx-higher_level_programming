f2c8b3cbdfdba06547aa433e072a8106484a37b7
Introvertedtechie <zillionaire234@gmail.com>
2023-03-13 16:59:08 +0100

    JS

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return a + b;
+}
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+if (!arg1 || !arg2 || isNaN(arg1) || isNaN(arg2)) {
+  console.log('Missing or invalid arguments');
+} else {
+  const result = add(parseInt(arg1), parseInt(arg2));
+  console.log(result);
+}
