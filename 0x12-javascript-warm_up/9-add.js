34f2a6d617d609af9c4778d5a930eaf8e24f38b6
Introvertedtechie <zillionaire234@gmail.com>
2023-03-13 17:02:55 +0100

    JS

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -8,7 +8,7 @@ const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
 if (!arg1 || !arg2 || isNaN(arg1) || isNaN(arg2)) {
-  console.log('Missing or invalid arguments');
+  console.log('NaN');
 } else {
   const result = add(parseInt(arg1), parseInt(arg2));
   console.log(result);
