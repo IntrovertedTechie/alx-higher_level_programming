1feaaeb433b4ef44a5894836ba409defbce92a01
Introvertedtechie <zillionaire234@gmail.com>
2023-03-14 23:59:42 +0100

    JS

--- 0x13-javascript_objects_scopes_closures/5.js
+++ 0x13-javascript_objects_scopes_closures/5.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
- if ((w <= 0) || (h <= 0) || !h || !w) {
+    if (w <= 0 || h <= 0) {
       // Create an empty object if width or height is not positive
       toString();
       return 'Rectangle {}';
