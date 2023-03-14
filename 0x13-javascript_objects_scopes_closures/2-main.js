4a46ad322a29a20ad068bd9e6e9f66101b6b5293
Introvertedtechie <zillionaire234@gmail.com>
2023-03-14 22:52:25 +0100

    JS

--- 0x13-javascript_objects_scopes_closures/2-main.js
+++ 0x13-javascript_objects_scopes_closures/2-main.js
@@ -11,12 +11,12 @@ console.log(r2);
 console.log(r2.width);
 console.log(r2.height);
 
-const r3 = new Rectangle(2);
+const r3 = new Rectangle(2,0);
 console.log(r3);
 console.log(r3.width);
 console.log(r3.height);
 
-const r4 = new Rectangle(2, 0);
+const r4 = new Rectangle(2,0);
 console.log(r4);
 console.log(r4.width);
 console.log(r4.height);
