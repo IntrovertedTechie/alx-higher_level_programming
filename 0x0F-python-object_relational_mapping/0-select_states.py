bfa4f28fdd6ae170339e46a061ddbae7f8e71f11
Introvertedtechie <zillionaire@gmail.com>
2023-03-18 19:22:23 +0100

    ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,34 +1,17 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa
+Lists all states from the database
+It takes 3 arguments: mysql username, mysql password and database name
 """
-
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == '__main__':
-    # take arguments from command line
-    mysql_username = "u0_a290"
-    mysql_password = ""
-    database_name = ""
-
-    # connect to MySQL server on localhost at port 3306
-    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
-
-    # create cursor to execute queries
-    cursor = db.cursor()
-
-    # execute query to get all states
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # fetch all the rows in a list of lists
-    rows = cursor.fetchall()
-
-    # print the rows
-    for row in rows:
-        print(row)
-
-    # close the cursor and database connection
-    cursor.close()
-    db.close()
-
+if __name__ == "__main__":
+	db = MySQLdb.connect(host="localhost",
+			user=argv[1], passwd=argv[2], db=argv[3])
+	cursor = db.cursor()
+	cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+	for state in cursor.fetchall():
+		print(state)
+	cursor.close()
+	db.close()
