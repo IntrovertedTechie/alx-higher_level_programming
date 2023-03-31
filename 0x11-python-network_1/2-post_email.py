ed992485a0c2da3c1389ccb888f3da913ebd6739
Introvertedtechie <zillionaire234@gmail.com>
2023-03-31 17:46:56 +0000

    python

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import urllib.request
-import urllib.parse
+""" takes in a URL and an email, sends a POST request  """
 import sys
+import urllib.request
+from urllib import parse
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    with urllib.request.urlopen(url, data=data) as response:
-        print(response.read().decode('utf-8'))
-
+    post_data = parse.urlencode({'email': sys.argv[2]}).encode('ascii')
+    with urllib.request.urlopen(url=sys.argv[1], data=post_data) as response:
+        data = response.read()
+        data = data.decode('ascii')
+        print(data)
