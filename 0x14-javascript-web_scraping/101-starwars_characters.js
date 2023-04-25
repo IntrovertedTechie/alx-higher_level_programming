950a8b7b7ad6801863fb93832a907ab722457ad1
Introvertedtechie <zillionaire234@gmail.com>
2023-04-25 13:55:20 +0000

    ₦£$€¥

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,8 +7,7 @@ const url = `https://swapi.dev/api/films/${movieId}/`;
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
-    return;
-  }
+     }
   const characters = JSON.parse(body).characters;
 
   printCharacters(characters, 0);
@@ -20,8 +19,7 @@ function printCharacters(characters, idx) {
   request(characters[idx], function (error, response, body) {
     if (error) {
       console.error(error);
-      return;
-    }
+        }
     console.log(JSON.parse(body).name);
     printCharacters(characters, idx + 1);
   });
