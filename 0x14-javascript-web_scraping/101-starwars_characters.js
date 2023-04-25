31c9ccf626d75c5dcdc03bb86b6962350dbe7cda
Introvertedtechie <zillionaire234@gmail.com>
2023-04-25 13:08:57 +0000

    ₦£$€¥

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,21 +1,27 @@
 #!/usr/bin/node
 const request = require('request');
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
-request(apiUrl, (error, response, body) => {
+request(url, function (error, response, body) {
   if (error) {
     console.error(error);
-  } else {
-    const charactersUrls = JSON.parse(body).characters;
-    charactersUrls.forEach((url) => {
-      request(url, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+    return;
   }
+  const characters = JSON.parse(body).characters;
+
+  printCharacters(characters, 0);
 });
+
+function printCharacters(characters, idx) {
+  if (idx >= characters.length) return;
+
+  request(characters[idx], function (error, response, body) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(JSON.parse(body).name);
+    printCharacters(characters, idx + 1);
+  });
+}
